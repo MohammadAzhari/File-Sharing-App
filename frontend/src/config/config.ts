54f99d868bc9668004235d2f7f2354390381fbce
Mohammad Azhari <mazharioffical@gmail.com>
2024-11-05 13:10:02 +0400

    fix base url default value

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import storage from "../utils/storage";
 
-const serverUrl = import.meta.env.API_URL || "http://localhost:4000";
+const serverUrl = import.meta.env.API_URL || "http://46.101.98.253:4000";
 
 const http = axios.create({
   baseURL: serverUrl,
